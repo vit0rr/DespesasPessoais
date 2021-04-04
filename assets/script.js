fad7beb1827e6bc316b67fc52c1436b278c16770
Vitor <vi.souza.almeida@protonmail.com>
2021-04-04 15:14:28 -0300

    Refatoração do modal

--- assets/script.js
+++ assets/script.js
@@ -65,8 +65,21 @@ function cadastrarDespesa() {
     )
     if (despesa.validarDados()) {
         bd.gravar(despesa)
-        $('#sucessoGravacao').modal('show')
+
+        document.getElementById('modal_titulo').innerHTML = 'Registro realizado'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso'
+        document.getElementById('modal_btn').innerHTML = 'Voltar'
+        document.getElementById('modal_btn').className = 'btn btn-success'
+        
+        $('#modalRegistraDespesa').modal('show')
     } else {
-        $('#erroGravacao').modal('show')
+        document.getElementById('modal_titulo').innerHTML = 'Erro na na inclusão do registro'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação! Verifique se todos os campos foram preenchidos corretamente'
+        document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+        document.getElementById('modal_btn').className = 'btn btn-danger'
+
+        $('#modalRegistraDespesa').modal('show')
     }
 }
